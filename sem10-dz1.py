8044f91df4625e37c9c57e5648bdb88b02c1255e
Totalpolarfox <totalpolarfox@outlook.com>
2024-02-06 18:25:12 +0300

    Modified task condition

--- sem10-dz1.py
+++ sem10-dz1.py
@@ -3,3 +3,10 @@
 Ваша задача перевести его в one hot вид. 
 Сможете ли вы это сделать без get_dummies?
 '''
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
